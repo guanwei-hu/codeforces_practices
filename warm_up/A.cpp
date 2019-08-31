79ebf2d4b6aceb4bdb83bfdfab06426156a7df04
HuGuanwei <guanwei.woo@gmail.com>
2019-09-01 02:58:20 +0800

    init

---
+++ warm_up/A.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+typedef long long int lld;
+
+int main() {
+	lld N, D;
+	lld x, a, g, r;
+	scanf_s("%lld %lld", &N, &D);
+	for (lld i = 0; i < N; i++) {
+		scanf_s("%lld %lld %lld %lld", &x, &a, &g, &r);
+		if (x < a || (x - a) % (g + r) > g) {
+			printf_s("NO");
+			return 0;
+		}
+	}
+	printf_s("YES");
+	return 0;
+}
