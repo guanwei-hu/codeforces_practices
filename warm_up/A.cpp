4af7dca8421a32647445f1f6f2b4913260ae2516
guanwei-hu <54144074+guanwei-hu@users.noreply.github.com>
2019-09-02 19:38:20 +0800

    Update A.cpp
    4-space tab

--- warm_up/A.cpp
+++ warm_up/A.cpp
@@ -2,10 +2,10 @@
 typedef long long int lld;
 
 int main() {
-	lld N, D;
-	lld x, a, g, r;
-	scanf_s("%lld %lld", &N, &D);
-	for (lld i = 0; i < N; i++) {
+    lld N, D;
+    lld x, a, g, r;
+    scanf_s("%lld %lld", &N, &D);
+    for (lld i = 0; i < N; i++) {
 		scanf_s("%lld %lld %lld %lld", &x, &a, &g, &r);
 		if (x < a || (x - a) % (g + r) > g) {
 			printf_s("NO");
